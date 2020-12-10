3338944827b7cd6da54cabbf7cf54701a8ee133d
Zack Broderson <zbroderson@hotmail.com>
2020-12-10 09:53:57 -0500

    Scaffold Interactive Service

--- Discord.Interactive/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Discord.Interactive
-{
-    public class Class1
-    {
-    }
-}
---
+++ Discord.Interactive/InteractiveService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Discord;
+using Discord.WebSocket;
+
+namespace Discord.Interactive
+{
+    public class InteractiveService
+    {
+        private BaseSocketClient Client { get; }
+
+        public TimeSpan DefaultTimeout { get; }
+
+        public InteractiveService(BaseSocketClient client, TimeSpan? defaultTimeout = null)
+        {
+            DefaultTimeout = defaultTimeout ?? TimeSpan.FromSeconds(15);
+        }
+
+        public InteractiveService(DiscordSocketClient client, TimeSpan? defaultTimeout = null)
+            : this(client as BaseSocketClient, defaultTimeout) { }
+
+        public InteractiveService(DiscordShardedClient client, TimeSpan? defaultTimeout = null)
+            : this(client as BaseSocketClient, defaultTimeout) { }
+
+
+
+    }
+}
