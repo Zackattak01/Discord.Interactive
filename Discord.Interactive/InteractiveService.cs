979cf664c7867d9c9817527e43d020d78947ac83
Zack Broderson <zbroderson@hotmail.com>
2020-12-13 20:53:26 -0500

    Primitive NextMessageAsync implementaion

--- Discord.Interactive/InteractiveService.cs
+++ Discord.Interactive/InteractiveService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 
@@ -21,6 +22,38 @@ namespace Discord.Interactive
 
         public InteractiveService(DiscordShardedClient client, TimeSpan? defaultTimeout = null)
             : this(client as BaseSocketClient, defaultTimeout) { }
+
+        
+        public Task<SocketMessage> NextMessageAsync(TimeSpan? timeout = null){
+            var socketMessageSource = new TaskCompletionSource<SocketMessage>();
+
+            var timeoutTask = Task.Delay(timeout ?? DefaultTimeout);
+            var socketMessageTask = socketMessageSource.Task;
+
+
+            Task MessageHandler(SocketMessage message) {
+                if(message.Author.Id == Client.CurrentUser.Id){
+                    return Task.CompletedTask;
+                }
+
+                socketMessageSource.SetResult(message);
+                return Task.CompletedTask;
+            }
+
+            try{
+                Client.MessageReceived += MessageHandler;
+
+                var firstTaskCompleted = Task.WhenAny(timeoutTask, socketMessageTask);
+
+                if(firstTaskCompleted == timeoutTask)
+                    return null;
+                else
+                    return socketMessageTask;
+            }
+            finally{
+                Client.MessageReceived -= MessageHandler;
+            }
+        }
 
 
 
